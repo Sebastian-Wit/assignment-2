f34b1cccf9bee4dd9299fcc42ab1c235c6f723fd
S <abc123@gmail.com>
2023-03-03 17:22:30 -0500

    Implemented functions

--- javascript-methods.js
+++ javascript-methods.js
@@ -10,50 +10,91 @@ In this Assignment, we use the prototype constructor to add new methods to the A
 
 // MAP //
 Array.prototype.myMap = function(callbackFn) {
-  // Place your code here.
+  const newArray = [];
+  for (let i = 0; i < this.length; i++){
+    newArray.push(callbackFn(this[i], i, this));
+  }
+  return newArray;
 };
 
 // FILTER //
 Array.prototype.myFilter = function(callbackFn) {
-  // Place your code here.
+  const newArray = [];
+  for (let i = 0; i < this.length; i++){
+    if (callbackFn(this[i], i, this)){
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
 };
 
 // SOME //
 Array.prototype.mySome = function(callbackFn) {
-  // Place your code here.
+  for (let i = 0; i < this.length; i++){
+    if (callbackFn(this[i], i, this)) return true;
+  }
+  return false;
 };
 
 // EVERY //
 Array.prototype.myEvery = function(callbackFn) {
-  // Place your code here.
+  for (let i = 0; i < this.length; i++){
+    if (!callbackFn(this[i], i, this)) return false;
+  }
+  return true;
 };
 
 // REDUCE //
 Array.prototype.myReduce = function(callbackFn) {
-  // Place your code here.
+  let accumulator = (initialValue === undefined ? undefined : initialValue);
+  for (let i = 0; i < this.length; i++) {
+    if (accumulator !== undefined){
+      accumulator = callbackFn.call(undefined, accumulator, this[i], i, this);
+    } else {
+      accumulator = this[i];
+    }
+  }
+  return accumulator;
 };
 
 // INCLUDES //
 Array.prototype.myIncludes = function(searchElement) {
-  // Place your code here.
+  for (let i = 0; i < this.length; i++){
+    if (this[i] === searchElement) return true;
+  }
+  return false;
 };
 
 // INDEXOF //
 Array.prototype.myIndexOf = function(searchElement) {
-  // Place your code here.
+  for (let i = 0; i < this.length; i++){
+    if (this[i] === searchElement) return i;
+  }
+  return -1;
 };
 
 // LASTINDEXOF //
 Array.prototype.myLastIndexOf = function(searchElement) {
-  // Place your code here.
+  for (let i = this.length - 1; i >= 0; i--){
+    if (this[i] === searchElement) return i;
+  }
+  return -1;
 };
 
 // KEYS //
 Object.myKeys = function(object) {
-  // Place your code here.
+  const keys = [];
+  for (let key in object){
+    if (object.hasOwnProperty(key)) keys.push(key);
+  }
+  return keys;
 };
 
 // VALUES //
 Object.myValues = function(object) {
-  // Place your code here.
+  const values = [];
+  for (let key in object){
+    if (object.hasOwnProperty(key)) values.push(object[key]);
+  }
+  return properties;
 };
